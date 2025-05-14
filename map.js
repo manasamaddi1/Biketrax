5fb502e4cda1226d2774b156109ccd05b0ff5848
manasamaddi1 <lmaddi@ucsd.edu>
2025-05-14 16:53:19 -0700

    map_added

--- map.js
+++ map.js
@@ -1,5 +1,19 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-// Check that Mapbox GL JS is loaded
-console.log('Mapbox GL JS Loaded:', mapboxgl);
+// Set your Mapbox access token
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFuYXNhbWFkZGkiLCJhIjoiY21hb2l5eDZnMDdzMzJpb2htNDBxYmd4ayJ9.Imf8ILwTblEUIw2wvv1yiw'; // Replace with your real token
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // The id of the HTML element
+  style: 'mapbox://styles/mapbox/streets-v12', // You can change this to another Mapbox style
+  center: [-71.0936142, 42.3591965], // Longitude, Latitude (MIT area)
+  zoom: 12
+});
+
+// Add zoom and rotation controls to the map
+map.addControl(new mapboxgl.NavigationControl());
+
+// Log confirmation
+console.log('Map initialized:', map);
